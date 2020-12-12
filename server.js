2657ac32c06f12e2883b29765118fcd08d31f6a9
Ron Nuqui <rooksfx@gmail.com>
2020-12-12 15:37:42 +0800

    Heroku Shenanigans

--- server.js
+++ server.js
@@ -26,9 +26,15 @@ app.use('/api/v1/components', components);
 app.use('/api/v1/build', build);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    // app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
+    app.use(express.static(path.join(__dirname, "client", "build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
 }
 
 const PORT = process.env.PORT || 5000;
