379747720b565e8e99d4b906363ceb75034bc5c6
Ron Nuqui <rooksfx@gmail.com>
2020-12-12 16:03:00 +0800

    Additional changes for this to run in Heroku

--- server.js
+++ server.js
@@ -25,16 +25,18 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/components', components);
 app.use('/api/v1/build', build);
 
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+// }
+
+// Serve static assets when in Prod
+
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static('client/build'));
+    app.use(express.static('client/build'));
 
-    // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-
-    app.use(express.static(path.join(__dirname, "client", "build")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
