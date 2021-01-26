ee14c51f42692e570a2631c478095ec198be1644
Ron Nuqui <rooksfx@gmail.com>
2021-01-26 23:49:39 +0800

    Added starting point

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BuildForm } from './BuildForm'
 import { Components } from './Components'
 import { AddComponent } from './AddComponent'
 
 export const Home = () => {
+    const [mode, setMode] = useState('')
+
     return (
         <div className="home">
             <img className="logo" src="../../logo.png" alt=""/>
-            <BuildForm />
-            <Components />
-            <AddComponent />
+            {(!mode && 
+                <div className='home-buttons'>
+                    <button onClick={() => setMode('build')}>BUILD</button>
+                    <button onClick={() => setMode('manage')}>MANAGE</button>
+                </div>
+            )}
+            {(mode === 'build' && 
+                <>
+                    <button className='back-btn' onClick={() => setMode('')}>BACK</button>
+                    <BuildForm />
+                </>
+            )}
+            {(mode === 'manage' && 
+                <>
+                    <button className='back-btn' onClick={() => setMode('')}>BACK</button>
+                    <Components />
+                    <AddComponent />
+                </>
+            )}
         </div>
     )
 }
